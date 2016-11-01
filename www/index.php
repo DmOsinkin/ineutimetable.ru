bb49c7cc65f26543ba2c2e08127b116237c24fac
DmOsinkin <dm.osinkin@gmail.com>
2016-11-01 14:12:12 +0300

    #task_3 демонстрация одного дня (структурированно, с часами пар)

--- www/index.php
+++ www/index.php
@@ -5,6 +5,31 @@ $db_user = 'root';  // Имя пользователя
 $db_password = '';  // Пароль пользователя
 $db_name = 'ineudb';          // Имя базы данных
 
+function getHours($i){
+    switch ($i) {
+        case 1:
+            echo "<tr><td><div align='center'>7.30-9.05</div></td>";
+            break;
+        case 2:
+            echo "<tr><td><div align='center'>9.20-10.55</div></td>";
+            break;
+        case 3:
+            echo "<tr><td><div align='center'>11.10-12.45</div></td>";
+            break;
+        case 4:
+            echo "<tr><td><div align='center'>13.15-14.50</div></td>";
+            break;
+        case 5:
+            echo "<tr><td><div align='center'>15.00-16.35</div></td>";
+            break;
+        case 6:
+            echo "<tr><td><div align='center'>16.45-18.20</div></td>";
+            break;
+        default:
+            break;
+    }
+}
+
 // Подключаемся к серверу
 $conn = mysql_connect($db_host, $db_user, $db_password) or die(
     "<p>Невозможно подключиться к СУБД: " . mysql_error() . ". Ошибка произошла в строке " . __LINE__ . "</p>");
@@ -23,10 +48,10 @@ $query = mysql_query("set names utf8", $conn) or die(
 $query = "SELECT * FROM lessons";
 
 //Запросы на каждый день недели (пока только понедельник - четверг)
-$mondayQuery = 'SELECT * FROM lessons WHERE `group`="13-САИ" AND `day`=1';
-$tuesdayQuery = 'SELECT * FROM lessons WHERE `group`="13-САИ" AND `day`=2';
-$wednesdayQuery = 'SELECT * FROM lessons WHERE `group`="13-САИ" AND `day`=3';
-$thursdayQuery = 'SELECT * FROM lessons WHERE `group`="13-САИ" AND `day`=4';
+$mondayQuery = 'SELECT * FROM lessons WHERE `group`="13-САИ" AND `day`=1 ORDER BY `number`';
+$tuesdayQuery = 'SELECT * FROM lessons WHERE `group`="13-САИ" AND `day`=2 ORDER BY `number`';
+$wednesdayQuery = 'SELECT * FROM lessons WHERE `group`="13-САИ" AND `day`=3 ORDER BY `number`';
+$thursdayQuery = 'SELECT * FROM lessons WHERE `group`="13-САИ" AND `day`=4 ORDER BY `number`';
 
 $mondayResult = mysqli_query($mondayQuery) or die('mondayQuery failed: ' . mysqli_error());
 $tuesdaResult = mysqli_query($tuesdayQuery) or die('tuesdayQuery failed: ' . mysqli_error());
@@ -47,18 +72,82 @@ echo '<html>
     <body>
 <table style="height:110px" width="600px"  border=\'2\'>';
 
+$mondayLessonArray = Array();
+$i = 0;
 while ($row = mysql_fetch_object($mondayResult)) {
-    echo
-        "<tr>
-            <td><div align='center'>row->number</div></td>
-            <td>
-                <div align='center' valign='top'>row->name<br></div>
-                <div align='left'>row->type<br></div>
-                <div align='left'>row->teacher<br></div>
-                <div align='left'>row->classroom</div>
-            </td>
-    </tr>";
+
+    $mondayLessonArray[$i] = array(
+        "number" => $row->number,
+        "first_week" => $row->first_week,
+        "second_week" => $row->second_week,
+        "name" => $row->name,
+        "classroom" => $row->classroom,
+        "type" => $row->type,
+        "teacher" => $row->teacher,
+    );
+    $i++;
 }
+
+for ($i = 0; $i < count($mondayLessonArray); $i++) {
+    if ($mondayLessonArray[$i]["first_week"] == $mondayLessonArray[$i]["second_week"]) {
+        getHours($mondayLessonArray[$i]["number"]);
+        echo
+        '<td>
+            <div align=\'center\' valign=\'top\'>', $mondayLessonArray[$i]["name"], '<br></div>
+            <div align=\'left\'>', $mondayLessonArray[$i]["type"], '<br></div>
+            <div align=\'left\'>', $mondayLessonArray[$i]["teacher"], '<br></div>
+            <div align=\'left\'>', $mondayLessonArray[$i]["classroom"], '</div>
+        </td>
+    </tr>';
+    } else {
+        for ($j = $i; $j < count($mondayLessonArray); $j++) {
+            if ($mondayLessonArray[$i]['number'] == $mondayLessonArray[$j]['number']){
+                getHours($mondayLessonArray[$i]["number"]);
+                if ($mondayLessonArray[$i]['first_week'] == 1){
+
+                    echo
+                    '<td>
+                        <table border=\'1\'> 
+                            <td style=\"width:250px\">
+                                <div align=\'center\' valign=\'top\'>'+$mondayLessonArray[$i]['name']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$i]['type']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$i]['teacher']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$i]['classroom']+'</div>
+                            </td>
+                            <td style=\"width:250px\">
+                                <div align=\'center\' valign=\'top\'>'+$mondayLessonArray[$j]['name']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$j]['type']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$j]['teacher']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$j]['classroom']+'</div>
+                            </td>
+                        </table>
+                    </td>';
+                } else {
+
+                    echo
+                        '<td>
+                        <table border=\'1\'>
+                            <td style=\"width:250px\">
+                                <div align=\'center\' valign=\'top\'>'+$mondayLessonArray[$j]['name']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$j]['type']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$j]['teacher']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$j]['classroom']+'</div>
+                            </td>
+                            <td style=\"width:250px\">
+                                <div align=\'center\' valign=\'top\'>'+$mondayLessonArray[$i]['name']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$i]['type']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$i]['teacher']+'<br></div>
+                                <div align=\'left\'>'+$mondayLessonArray[$i]['classroom']+'</div>
+                            </td>
+
+                        </table>
+                    </td>';
+                }
+            }
+        }
+    }
+}
+
 echo '</table>
 </body>
 </html>';
