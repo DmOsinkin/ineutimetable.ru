0d6382bed819116832dab8c0ab31667990602fd8
DmOsinkin <dm.osinkin@gmail.com>
2016-11-29 14:03:19 +0300

    работа над комбобоксом (проблема с областью видимости переменных)

--- config.php
+++ config.php
@@ -1,9 +1,21 @@
 <?
 
+global $db_host;
 $db_host = 'localhost';     // Сервер
+global $db_user;
 $db_user = 'root';  // Имя пользователя
+global $db_password;
 $db_password = '';  // Пароль пользователя
+global $db_name;
 $db_name = 'ineudb';          // Имя базы данных
+
+// Подключаемся к серверу
+    $conn = mysqli_connect($db_host, $db_user, $db_password) or die(
+                    "<p>Невозможно подключиться к СУБД: " . mysqli_error() . ". Ошибка произошла в строке " . __LINE__ . "</p>");
+
+    $db = mysqli_select_db($conn, $db_name) or die(
+                    "<p>Невозможно подключиться к базе данных: " . mysqli_error() . ". Ошибка произошла в строке " . __LINE__ . "</p>");
+
 
 //Запросы на каждый день недели (пока только понедельник - четверг)
 $mondayQuery = 'SELECT * FROM lessons WHERE `group`="13-САИ" AND `day`=1 ORDER BY `number`';
--- readGroup.php
+++ readGroup.php
@@ -1,8 +1,10 @@
 <?php
-require 'index.php';
 
+require 'index.php';
 if ($_POST['group'] == '13sai') {
-    require './database.php';
+require 'config.php';
+
+
 } else {
     echo 'nothing';
 }
