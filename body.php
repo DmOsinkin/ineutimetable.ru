e8c51c202d29b5c3d55a7ced6348f390df3329cb
Дмитрий Осинкин <dm.osinkin@gmail.com>
2016-12-13 00:52:07 +0300

    #добавлен пароль для доступа к БД

--- body.php
+++ body.php
@@ -1,7 +1,7 @@
 <?php 
 $db_host = 'localhost';     // Сервер
 $db_user = 'root';  // Имя пользователя
-$db_password = '';  // Пароль пользователя
+$db_password = 'LVBNHBQ';  // Пароль пользователя
 $db_name = 'ineudb';          // Имя базы данных
 
 // Подключаемся к серверу
--- timetable.php
+++ timetable.php
@@ -4,7 +4,7 @@ require 'index.php';
 
 $db_host = 'localhost';     // Сервер
 $db_user = 'root';  // Имя пользователя
-$db_password = '';  // Пароль пользователя
+$db_password = 'LVBNHBQ';  // Пароль пользователя
 $db_name = 'ineudb';          // Имя базы данных
 
 function getHours($_hour) {
